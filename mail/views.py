5838a5ed942f4e709d434ad45ad13851259bffdb
VitoNick <vitonick81@gmail.com>
2025-12-01 19:26:08 -0800

    Project strudture additions before starting specs and pulling to new MacBook

--- mail/views.py
+++ mail/views.py
@@ -2,8 +2,8 @@ import json
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import JsonResponse
-from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
