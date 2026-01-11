52ede5cae73b1f2094ec7718f60c3f709266c688
Matt Nicassio <vitonick81@gmail.com>
2026-01-10 22:17:31 -0800

    sent emails displaying (need to fix styling)

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -28,9 +28,22 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
+  
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  // load mailbox
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      // Print emails
+      console.log(emails);
+      // list emails
+      document.querySelector('#emails-view').innerHTML += '<ul>';
+      emails.forEach(email => {
+        document.querySelector('#emails-view').innerHTML += `<li>${email.subject} - ${email.sender} - ${email.timestamp}</li>`;
+      });
+    });
 } 
 
 function send_email() {
